6a7cd83be550721359c33ac691a2d534882fa4b4
admin <hi.chuntachen@gmail.com>
2020-08-08 19:47:29 +0800

    Initial Commit

---
+++ OrtDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OrtDemo
+//
+//  Created by apple on 2020/8/8.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
